f8e9abba6814e70013f2bacd80e8144e5f961c0e
Shraavya <mishrashraavya@gmail.com>
2026-07-23 15:38:44 -0700

    do npb results

--- NPB/NPB3.4/NPB3.4-MPI/BT/npbparams.h
+++ NPB/NPB3.4/NPB3.4-MPI/BT/npbparams.h
@@ -1,4 +1,4 @@
-c CLASS = A
+c CLASS = B
 c  
 c  
 c  This file is generated automatically by the setparams utility.
@@ -6,9 +6,9 @@ c  It sets the number of processors and the class of the NPB
 c  in this directory. Do not modify it by hand.
 c  
         integer problem_size, niter_default
-        parameter (problem_size=64, niter_default=200)
+        parameter (problem_size=102, niter_default=200)
         double precision dt_default
-        parameter (dt_default = 0.0008d0)
+        parameter (dt_default = 0.0003d0)
         integer wr_default
         parameter (wr_default = 5)
         integer iotype
